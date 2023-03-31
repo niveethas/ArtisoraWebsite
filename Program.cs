bd55e165a387dc334e8fea4b7152166b53907620
niveethas <61502706+niveethas@users.noreply.github.com>
2023-03-31 12:42:09 +0100

    API Dependency Injection

--- Program.cs
+++ Program.cs
@@ -2,12 +2,14 @@ using ArtisoraWebsite;
 using Microsoft.AspNetCore.Components.Web;
 using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
 using Radzen;
+using ArtisoraServer;
 
 var builder = WebAssemblyHostBuilder.CreateDefault(args);
 builder.RootComponents.Add<App>("#app");
 builder.RootComponents.Add<HeadOutlet>("head::after");
 
 builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
+builder.Services.AddScoped<ClientAPI>(sp => new ClientAPI($"https://localhost:7123/", new HttpClient()));
 builder.Services.AddScoped<DialogService>();
 builder.Services.AddScoped<NotificationService>();
 builder.Services.AddScoped<TooltipService>();
