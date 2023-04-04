06ed5895d68de4deef598889ca3dff4345a90446
niveethas <61502706+niveethas@users.noreply.github.com>
2023-04-04 23:11:51 +0100

    global user class added

---
+++ CurrentUser.cs
@@ -0,0 +1,15 @@
+﻿namespace ArtisoraWebsite
+{
+    public class CurrentUser
+    {
+      
+            public static int? _role;
+            public static int? _userid;
+            public static string? _useremail;
+
+            public int? Role { get { return _role; } set { _role = value; } }
+            public int? UserId { get { return _userid; } set { _userid = value; } }
+            public string UserEmail { get { return _useremail; } set { _useremail = value; } }
+    }
+
+}
